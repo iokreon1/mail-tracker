4a31fd6c8865685016a825cde4424da4b3476d2f
iokreon1 <wahyuhariyanto157@gmail.com>
2025-11-12 13:37:36 +0700

    menambahkan header

---
+++ app/views/layouts/header.php
@@ -0,0 +1,20 @@
+<div class="navbar-bg"></div>
+<nav class="navbar navbar-expand-lg main-navbar">
+    <form classs="form-inline mr-auto">
+        <ul class="navbar-nav mr-3">
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+        </ul>
+    </form>
+    <ul class="navbar-nav navbar-right">
+        <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+            <i class="fa fas-user rounded-circle mr-1"></i>
+            <div class="d-sm-none d-lg-inline-block">Hi, <?= $_SESSION['user']['name'] ?></div>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right">
+            <a href="<?= BASEURL ?>AuthController/logout" class="dropdown-item has-icon text-danger">
+                <i class="fas fa-sign-out-alt"></i> Logout
+            </a>
+        </div>
+    </li>
+    </ul>
+</nav>
